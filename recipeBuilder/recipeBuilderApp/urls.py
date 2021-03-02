46fd81d76628162d23b9218055ec91ef653171c2
alauran <alauran@g.clemson.edu>
2021-03-01 19:58:33 -0500

    Create urls.py

---
+++ recipeBuilder/recipeBuilderApp/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url, include
+from django.urls import path
+
+from recipeBuilderApp.views import home
+
+
+
+
+urlpatterns = [
+    url(r"^home/", Recipe-Builder_Home, name="Recipe-Builder_Home"),
+
+
+
+]
