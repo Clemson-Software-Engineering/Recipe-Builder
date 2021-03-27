1bcf147b7c09ae6244aaf87b88b87fa46a95751c
Emily Williams <56053468+Eaw7@users.noreply.github.com>
2021-03-26 22:08:59 -0400

    Added basic Sign Up landing page

---
+++ recipeBuilder/recipeBuilderApp/templates/Recipe-Builder_SignUp_Success.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8">
+<title>Recipe-Builder | Sign Up Success!</title>
+</head>
+
+<body>
+
+<?php
+
+echo "<h1>Welcome " . $_POST["username"] . "!</h1>";
+echo "<p>Your account is now active.</p>";
+
+?>
+
+</body>
+</html>
